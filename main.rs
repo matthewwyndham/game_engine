4fc44ad450776d056702edeb16a25bef3101ae90
Matt Wyndham <matthewwyndham@gmail.com>
2022-02-03 16:06:19 -0700

    bevy

--- main.rs
+++ main.rs
@@ -57,6 +57,8 @@ Features:
         - Beat the boss on normal mode, unlock hard mode: faster & stronger enemies, more money, better loot, faster gameplay. 
         - Multiple difficulty levels. 
         - Final unlock is creative mode where you can experiment with different item combos.
+
+- Robot survival game in junkworld
 */
 
 // roguelike game first
@@ -68,9 +70,7 @@ fn main() {
     println!("...main...");
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+        .add_plugin(HelloPlugin)
         .run();
     // init game
 
@@ -87,10 +87,6 @@ fn main() {
     // game loop
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
-
 #[derive(Component)]
 struct Person;
 
@@ -103,8 +99,23 @@ fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app
+        .insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+        .add_startup_system(add_people)
+        .add_system(greet_people);
     }
 }
